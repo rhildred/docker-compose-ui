5605eaa3ed03a57a4ce2262d405fa9decf366102
Rich Hildred <rhildred@gmail.com>
2019-01-10 22:47:16 -0500

    got the port both in the .env file and in the request

--- static/scripts/controllers/create.js
+++ static/scripts/controllers/create.js
@@ -8,113 +8,123 @@
  * Controller of the composeUiApp
  */
 angular.module('composeUiApp')
-  .controller('CreateCtrl', function ($scope, $routeParams, $resource, $location) {
+    .controller('CreateCtrl', function ($scope, $routeParams, $resource, $location) {
 
-    var isEdit = $location.path().indexOf('/edit/') === 0;
+        var isEdit = $location.path().indexOf('/edit/') === 0;
 
-    if (isEdit) {
-        $scope.name = $routeParams.from;
-        $scope.isEdit = true;
-    }
+        if (isEdit) {
+            $scope.name = $routeParams.from;
+            $scope.isEdit = true;
+        }
 
-      var Projects = $resource('api/v1/projects', null, {
+        var Projects = $resource('api/v1/projects', null, {
 
-          'createProject' : {
-            url: 'api/v1/create-project',
-            method:  'POST'
-          },
-          'updateProject': {
-            url: 'api/v1/update-project',
-            method: 'PUT'
+            'createProject': {
+                url: 'api/v1/create-project',
+                method: 'POST'
             },
-          'loadProject': {
-              url: 'api/v1/projects/yml/:id',
-              method: 'GET'
-          }
-      });
+            'updateProject': {
+                url: 'api/v1/update-project',
+                method: 'PUT'
+            },
+            'loadProject': {
+                url: 'api/v1/projects/yml/:id',
+                method: 'GET'
+            }
+        });
 
-      var Search = $resource('api/v1/search');
-      var Yml = $resource('api/v1/yml');
+        var Search = $resource('api/v1/search');
+        var Yml = $resource('api/v1/yml');
 
-      var ComposeRegistry = $resource('api/v1/compose-registry');
+        var ComposeRegistry = $resource('api/v1/compose-registry');
 
-      ComposeRegistry.get(function (data) {
-          var url = data.url;
-          $scope.composeRegistry = url;
-          if (url) {
-            $scope.search('yml');
-          }
-      });
+        ComposeRegistry.get(function (data) {
+            var url = data.url;
+            $scope.composeRegistry = url;
+            if (url) {
+                $scope.search('yml');
+            }
+        });
 
-      $scope.createProject = function (name, repoName, webhookurl, env) {
+        $scope.createProject = function (name, repoName, webhookurl, env) {
 
-      //TODO: check if name is alphanumeric
-          Projects[isEdit ? 'updateProject' : 'createProject']({
-              name: name,
-              repoName: repoName,
-              webhookurl, webhookurl,
-              env: env
-          }, function (data) {
-              alertify.success((isEdit ? 'updated project' : 'created project: ') + name + ', path: ' + data.path);
-              $scope.$parent.reload(false);
-              $location.path('project/' + name);
+            //TODO: check if name is alphanumeric
+            //add our special string to the env
 
-          }, function (err) {
-              alertify.alert(err.data);
-          });
+            var sHostName = window.location.hostname.replace("apps", name);
+            var sPort = crc16(sHostName);
+            var sEnv = "RHPORT=" + sPort + "\n";
+            if(env){
+                sEnv += env;
+            }
+            console.log("host is " + sHostName + " port is " + sPort);
 
+            Projects[isEdit ? 'updateProject' : 'createProject']({
+                name: name,
+                repoName: repoName,
+                webhookurl, webhookurl,
+                env: sEnv
+            }, function (data) {
+                alertify.success((isEdit ? 'updated project' : 'created project: ') + name + ', path: ' + data.path);
+                $scope.$parent.reload(false);
+                $location.path('project/' + name);
 
-      };
-
-      // pre-fill a project's details
-      $scope.loadProject = function (projectId) {
-
-          Projects.loadProject({ id: projectId }, function (data) {
-              if ('yml' in data) {
-                  $scope.yml = data.yml;
-              }
-              if ('env' in data) {
-                  $scope.env = data.env;
-              }
-          }, function (err) {
-              alertify.alert(err.data);
-          });
-
-      };
-
-      if ('from' in $routeParams) {
-          $scope.loadProject($routeParams.from);
-      }
+            }, function (err) {
+                alertify.alert(err.data);
+            });
 
 
-      $scope.search = function (query) {
+        };
 
-          Search.save({
-              query: query
-          }, function (res) {
-              $scope.items = res.items;
-          }, function (res) {
-              alertify.alert('search error: ' + res.data.error);
-          });
+        // pre-fill a project's details
+        $scope.loadProject = function (projectId) {
 
-      };
+            Projects.loadProject({ id: projectId }, function (data) {
+                if ('yml' in data) {
+                    $scope.yml = data.yml;
+                }
+                if ('env' in data) {
+                    $scope.env = data.env;
+                }
+            }, function (err) {
+                alertify.alert(err.data);
+            });
 
+        };
 
-
-      $scope.load = function (name, id) {
-
-          Yml.save({
-              id: id
-          }, function (data) {
-              $scope.yml = data.content;
-          });
-          $scope.name = name;
-          $scope.items = [];
-      };
-
-      $scope.webhookurl = function() {
-          return window.location.href.replace("#/create", "webhooks/" + $scope.name);
-      }
+        if ('from' in $routeParams) {
+            $scope.loadProject($routeParams.from);
+        }
 
 
-  });
+        $scope.search = function (query) {
+
+            Search.save({
+                query: query
+            }, function (res) {
+                $scope.items = res.items;
+            }, function (res) {
+                alertify.alert('search error: ' + res.data.error);
+            });
+
+        };
+
+
+
+        $scope.load = function (name, id) {
+
+            Yml.save({
+                id: id
+            }, function (data) {
+                $scope.yml = data.content;
+            });
+            $scope.name = name;
+            $scope.items = [];
+        };
+
+        $scope.webhookurl = function () {
+            return window.location.href.replace("#/create", "webhooks/" + $scope.name);
+        }
+
+
+    });
