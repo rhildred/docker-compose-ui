8fa9171d420e752e4d29c31d5c2cb537fe75e372
Rich Hildred <rhildred@gmail.com>
2019-01-11 20:28:30 -0500

    combined getting ip with the app_server

--- app_server.py
+++ app_server.py
@@ -1,11 +1,38 @@
 import bjoern
 import os
 import signal
+import fileinput
+import socket
+
+#first write ip address of this server for nginx config
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+sIp = get_ip()
+oFile = open("nginx.conf", "w")
+for line in fileinput.input(['nginx.tmpl.conf']):
+    oFile.write(line.replace('dockerhost', sIp))
+oFile.close()
+
+#we will want to run docker-compose up equivalent here
+
+#now set up as a web server
+
 from main import app
 
 host = '0.0.0.0'
 port = 5001
-NUM_WORKERS = 2
+NUM_WORKERS = 5
 worker_pids = []
 
 
