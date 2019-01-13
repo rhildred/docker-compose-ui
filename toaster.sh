9880bafbd678aba12e68ec45f132b91751f1fc3b
Richard Hildred <rhildred@gmail.com>
2019-01-13 14:48:35 -0500

    trying --user

--- toaster.sh
+++ toaster.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -i
 sudo yum update
 sudo yum install python3 libev docker git
-sudo pip3 install docker-compose
+pip3 install --user docker-compose
 git clone https://github.com/rhildred/docker-compose-ui.git
 cd docker-compose-ui
-sudo pip3 install -r requirements.txt
+pip3 install --user -r requirements.txt
 python3 setup.py
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 bash -c "source ~/.nvm/nvm.sh ; nvm install node; npm install -g pm2; pm2 start app_server.py --interpreter=python3"
