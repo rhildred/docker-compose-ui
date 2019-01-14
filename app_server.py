efc38f36baf2416e5ef75e7e4cfca4cce7b443f8
Rich Hildred <rhildred@gmail.com>
2019-01-14 09:41:46 -0500

    moved startup stuff from app_server to setup.py

--- app_server.py
+++ app_server.py
@@ -3,32 +3,7 @@ import os
 import signal
 import fileinput
 import socket
-from scripts.bridge import get_project
 
-#first write ip address of this server for nginx config
-
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
-
-sIp = get_ip()
-oFile = open("nginx.conf", "w")
-for line in fileinput.input(['nginx.tmpl.conf']):
-    oFile.write(line.replace('dockerhost', sIp))
-oFile.close()
-
-#we will want to run docker-compose up equivalent here
-sPath = os.path.dirname(os.path.abspath(__file__))
-get_project(sPath).up()
-#now set up as a web server
 
 from main import app
 
