1e2af78d96ac8ee03592aea5605f1dffb5068369
Richard Hildred <rhildred@gmail.com>
2019-01-13 19:15:45 -0500

    added code to create self signed certificate

--- toaster.sh
+++ toaster.sh
@@ -11,3 +11,4 @@ pip3 install --user -r requirements.txt
 python3 setup.py
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 bash -c "source ~/.nvm/nvm.sh ; nvm install node; npm install -g pm2; pm2 start app_server.py --interpreter=python3"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout localhost.key -out localhost.crt
